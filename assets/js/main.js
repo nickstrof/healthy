a2728410e2f68800b5119dcba3e9c1b63c432e9a
nickstrof <strofyllasnikolaos@gmail.com>
2025-05-03 20:00:32 +0300

    Update header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
+// General
+
+const body = document.body;
+
 // Menu Dropdown
 
 const menuToggle = document.getElementById("menu-toggle");
 const menuDropdown = document.querySelector(".menu-dropdown");
 const blackModal = document.querySelector(".black-modal");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuToggle.addEventListener('click', function (e) {
     menuDropdown.classList.toggle('active');
     blackModal.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
 });
 
 const closeMenu = function () {
@@ -21,5 +27,30 @@ document.addEventListener('click', e => {
     if(blackModal.contains(e.target)) {
         blackModal.classList.add('active');
         closeMenu();
+    }
+});
+
+window.addEventListener('resize', () => {
+    menuDropdown.classList.remove('active');
+    blackModal.classList.remove('active');
+    mobileMenu.classList.remove('active');
+})
+
+
+// Color mode
+
+const colorMode = document.querySelector(".c-mode");
+
+if(localStorage.getItem('theme') === 'light') {
+    body.classList.add('light');
+}
+
+colorMode.addEventListener('click', () => {
+    body.classList.toggle('light');
+
+    if(body.classList.contains('light')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', '');
     }
 });
