c71f2a76498efd5499dfd7ea06dcccc59987b0e1
nickstrof <strofyllasnikolaos@gmail.com>
2025-05-15 02:10:19 +0300

    create sections hero, categories, about and about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,3 +54,25 @@ colorMode.addEventListener('click', () => {
         localStorage.setItem('theme', '');
     }
 });
+
+
+// SVG Paths
+
+document.addEventListener("DOMContentLoaded", () => {
+    const paths = document.querySelectorAll('#myPath path');
+
+    paths.forEach(path => {
+        const length = path.getTotalLength();
+        path.style.strokeDasharray = length;
+        path.style.strokeDashoffset = length;
+
+        window.addEventListener("scroll", () => {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+            const scrollPercent = scrollTop / docHeight;
+            const drawLength = length * Math.min(scrollPercent * 1, 1);
+
+            path.style.strokeDashoffset = length - drawLength;
+        });
+    });
+});
